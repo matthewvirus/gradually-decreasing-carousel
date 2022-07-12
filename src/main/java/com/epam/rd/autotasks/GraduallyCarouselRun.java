1597a91d4fbb85b5d6295417cae3f1b2b924274a
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-07-12 23:02:06 +0300

    solved

---
+++ src/main/java/com/epam/rd/autotasks/GraduallyCarouselRun.java
@@ -0,0 +1,39 @@
+package com.epam.rd.autotasks;
+
+public class GraduallyCarouselRun extends CarouselRun {
+
+    private final int[] array = GraduallyDecreasingCarousel.carouselArray;
+    private int position;
+    private int decrement = 1;
+
+    @Override
+    public int next() {
+        if (isFinished()) {
+            return -1;
+        }
+        while (array[position] <= 0) {
+            if (position >= array.length - 1) {
+                position = 0;
+                decrement++;
+                continue;
+            }
+            position++;
+        }
+        int currentElement = array[position];
+        array[position] -= decrement;
+        position++;
+        if (position == array.length) {
+            position = 0;
+            decrement++;
+        }
+        return currentElement;
+    }
+
+    @Override
+    public boolean isFinished() {
+        for (int var: array) {
+            if (var > 0) return false;
+        }
+        return true;
+    }
+}
--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,7 +1,33 @@
 package com.epam.rd.autotasks;
 
 public class GraduallyDecreasingCarousel extends DecrementingCarousel{
+
+    private boolean isCarouselRunning = false;
+    private int carouselRunCounter = 0;
+    static int[] carouselArray;
+    int counter = 0;
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
+        carouselArray = new int[capacity];
+    }
+
+    @Override
+    public boolean addElement(int element) {
+        if (counter < capacity && element > 0 && !isCarouselRunning) {
+            carouselArray[counter] = element;
+            counter++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public CarouselRun run() {
+        if (carouselRunCounter == 0) {
+            isCarouselRunning = true;
+            carouselRunCounter++;
+            return new GraduallyCarouselRun();
+        }
+        return null;
     }
 }
